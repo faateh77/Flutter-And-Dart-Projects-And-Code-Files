b852577abcd813840e4f5233b674506b208bc869
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-26 00:34:01 +0500

    Add files via upload

---
+++ Dart Assignment 1/question4.dart
@@ -0,0 +1,11 @@
+void main() {
+int eng =65;
+int maths  =76;
+int comp = 84;
+int total = 300;
+var obt = eng + maths + comp;
+var per = obt * 100 / total;
+print("obtained marks: $obt");
+print("total marks: $total");
+print("your percentage is: $per");
+}
