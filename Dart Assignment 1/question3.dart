795ad9bafdd7316ae1c8ca77819b818e38aab76b
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-26 00:31:26 +0500

    Add files via upload

---
+++ Dart Assignment 1/question3.dart
@@ -0,0 +1,11 @@
+void main() {
+//If both conditions are true
+var a  = 2;
+var b = 4;
+print(a <50 && a <b);
+
+//If any one of condition are true
+var c  = 12;
+var d= 7;
+print(c <50 || c <d);
+}
