2d451ff649ba0dce74090b85cf1d7cc7e5111f4a
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-26 12:01:17 +0500

    Add files via upload

---
+++ Dart Practice Codes/variables.dart
@@ -0,0 +1,13 @@
+void main(){
+  var name2 = "Ali";
+  int number  = 2;
+  double decimalNumber = 4;
+  String name = "rehan";
+  bool condition = true;
+  print(name);
+  print(decimalNumber);
+   
+  print(number);
+  print(name2);
+  print(condition);
+}
