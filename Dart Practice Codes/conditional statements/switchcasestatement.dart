1650171e72ba7c46f24a5644fbf29ef91fde7ab8
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-27 02:38:08 +0500

    Add files via upload

---
+++ Dart Practice Codes/conditional statements/switchcasestatement.dart
@@ -0,0 +1,26 @@
+void main(){
+  String grade = "G";
+  switch(grade){
+    case 'A':
+       print("Your grade is A");
+       break;
+    case 'B':
+       print("Your grade is B");
+       break;
+    case 'C':
+       print("Your grade is C");
+       break;
+    case 'D':
+       print("Your grade is D");
+       break;
+    case 'E':
+       print("Your grade is E");
+       break;
+    case 'F':
+       print("Your grade is F");
+       break;   
+    default:
+       print("Invalid grade");
+    
+  }
+}
