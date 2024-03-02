03ad858514f777a0fc85fd1eb05294ac999b1066
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-02 09:43:47 +0500

    Add files via upload

---
+++ Dart Assignment 2/question6.dart
@@ -0,0 +1,9 @@
+void main(){
+  String letter = 'n';
+  if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+    print("Letter is vowel");
+  }
+  else{
+    print("Letter is consonant");
+  }
+}
