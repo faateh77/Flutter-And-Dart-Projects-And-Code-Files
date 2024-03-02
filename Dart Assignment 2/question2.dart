4db32e16ee5ffcd6d2481138ff116c2f750235cf
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-02 08:57:10 +0500

    Add files via upload

---
+++ Dart Assignment 2/question2.dart
@@ -0,0 +1,10 @@
+void main(){
+  var age = 14;
+  var age2 = 20;
+  if(age > age2){
+    print ("Person 1 is older than Person 2");
+  }
+  else{
+    print ("Person 2 is older than Person 1");
+  }
+}
