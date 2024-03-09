ca63be7274acf24be73f726310849a95d8b28eb4
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-09 09:59:45 +0500

    Add files via upload

---
+++ Dart Assignment 3/question2.dart
@@ -0,0 +1,12 @@
+void main(){
+  var character = '@';
+  if(character == 'a' || character == 'b' || character == 'c'  || character == 'd'  || character == 'e' || character == 'f'  || character == 'g' || character == 'h'  || character == 'i' || character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n' || character == 'o' || character == 'p' || character == 'r' || character == 's' || character == 't' || character == 'u' || character == 'v' || character == 'w' || character == 'x' || character == 'y' || character == 'z'  ){
+    print("character is letter");
+  }
+  else if( character == '@' || character == '%'|| character == '&'  ){
+    print("character is special letter");
+  }
+  else {
+    print("character is number");
+  }
+}
