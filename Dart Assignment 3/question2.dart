3ad35cdf5ab65e9eed446aee7003754036167824
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-09 10:24:21 +0500

    Add files via upload

--- Dart Assignment 3/question2.dart
+++ Dart Assignment 3/question2.dart
@@ -3,7 +3,7 @@ void main(){
   if(character == 'a' || character == 'b' || character == 'c'  || character == 'd'  || character == 'e' || character == 'f'  || character == 'g' || character == 'h'  || character == 'i' || character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n' || character == 'o' || character == 'p' || character == 'r' || character == 's' || character == 't' || character == 'u' || character == 'v' || character == 'w' || character == 'x' || character == 'y' || character == 'z'  ){
     print("character is letter");
   }
-  else if( character == '@' || character == '%'|| character == '&'  ){
+  else if( character == '@' || character == '%'|| character == '&' || character == '#' || character == '^' || character == '*'  ){
     print("character is special letter");
   }
   else {
