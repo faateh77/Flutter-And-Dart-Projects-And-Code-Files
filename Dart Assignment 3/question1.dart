820645406508708ebc2373b41ff5a752281115e6
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-09 09:44:52 +0500

    Add files via upload

---
+++ Dart Assignment 3/question1.dart
@@ -0,0 +1,12 @@
+void main(){
+  int number = 0;
+  if(number > 0){
+    print("Number is positive");
+  }
+  else if(number < 0 ){
+    print("Number is negative");
+  }
+  else{
+    print("Number is zero");
+  }
+}
