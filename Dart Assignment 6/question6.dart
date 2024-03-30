946216da527d050624c0af392edc4b349f02d465
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-30 10:58:07 +0500

    Add files via upload

---
+++ Dart Assignment 6/question6.dart
@@ -0,0 +1,10 @@
+void main() {
+  Map nameWorld = {
+    'countries': [
+      'Pakistan:{capitalCity:Islamabad,currency:Rupee}',
+      'Bangladesh:{capitalCity:Dhaka,currency:Taka}',
+      'Japan:{capitalCity:Tokyo,currency:Yen}',
+    ],
+  };
+  print(nameWorld.keys);
+}
